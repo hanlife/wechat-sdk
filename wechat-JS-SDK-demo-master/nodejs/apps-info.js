7fc2658ddda107909e13d02bc01e1f4ff454bbf2
hanlife <m13760315374@163.com>
2018-07-26 15:39:16 +0800

    first

---
+++ wechat-JS-SDK-demo-master/nodejs/apps-info.js
@@ -0,0 +1,17 @@
+/**
+ * 微信认证后的 app 信息
+ * 如果有需要可以将这些信息放到数据库中读取方便以更增加
+ * 注：测试时请替换您真实的appid和secret
+ */
+module.exports = function(){
+	return [
+			{
+				appid: 'wxc85dbc62dd24d971'
+				,secret: 'bbe94b9536e691157b0bd71fb51ee1e7'
+			},
+			{
+				appid: 'wx2967c49b49910205'
+				,secret: '809146feabe2780b62e9de94f78ae7b7'
+			}
+		];
+};
